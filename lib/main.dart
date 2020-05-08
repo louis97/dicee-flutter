6fb3266b2f64c9793b1ce9f0cd9d976ca92f1a21
Louis Gualtero <lf.gualtero@uniandes.edu.co>
2020-05-07 22:08:49 -0500

    latest commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,6 +8,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -15,9 +18,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int die1= new Random().nextInt(6) + 1;
+  int die2= new Random().nextInt(6) + 1;
+  void f(){setState(() {
+    die1 = new Random().nextInt(6) + 1;
+    die2 = new Random().nextInt(6) + 1;
+  });
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    f();
+                  });
+                },
+                child: Image.asset('images/dice$die1.png'),
+              ),
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    f();
+                  });
+                },
+                child: Image.asset('images/dice$die2.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
